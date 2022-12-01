772a2af37e4202a48077f33e0ffb15fb78055482
tetianasabanina <tatiana08st@gmail.com>
2022-12-01 20:44:20 +0200

    initial commit, install dependencies

---
+++ src/components/NewTransaction.tsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import { transactionListState } from '../recoil/atoms';
+import { useRecoilState } from 'recoil';
+import { v4 as uuidv4 } from 'uuid';
+
+export const NewTransaction = () => {
+	const [textValue, setTextValue] = useState('');
+	const [amount, setAmount] = useState(0);
+	// const setTransactionList = useRecoilState(transactionListState);
+	const [transactionList, setTransactionList] =
+		useRecoilState(transactionListState);
+
+	const NewTransaction = (e) => {
+		e.preventDefault();
+		setTransactionList((oldTransactionList) => [
+			...oldTransactionList,
+			{
+				id: uuidv4(),
+				amount: +amount,
+				text: textValue,
+			},
+		]);
+		setTextValue('');
+		setAmount(0);
+	};
+
+	useEffect(() => {
+		localStorage.setItem('TRANSACTION_LIST', JSON.stringify(transactionList));
+	}, [transactionList]);
+
+	const handleTextValue = (e) => {
+		setTextValue(e.target.value);
+	};
+	const handleAmount = (e) => {
+		setAmount(e.target.value);
+	};
+
+	return (
+		<>
+			<h3>Add new transaction</h3>
+			<form>
+				<div>
+					<label htmlFor='text'>Description</label>
+					<input value={textValue} onChange={handleTextValue} />
+				</div>
+				<div>
+					<label htmlFor='amount'>
+						Amount <br />
+					</label>
+					<input value={amount} onChange={handleAmount} />
+				</div>
+				<button onClick={NewTransaction}>Add transaction</button>
+			</form>
+		</>
+	);
+};
